76815fe58d485dd7b16e0f49c6df4cef59f5a291
kellydhunter1 <kellydhunter1@gmail.com>
2021-04-26 12:45:51 -0400

    update js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,16 @@
-// this allows user to create a name
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("Our robot's name is " + playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-// this creates a function named "fight"
-function fight() {
-    window.alert("The fight has begun!");
-  }
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-//fight();
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+};
+
+fight();
